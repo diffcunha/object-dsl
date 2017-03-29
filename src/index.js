c13e3a9630839a3a3a91a76aaeca7ead644bb6fc
Diogo Cunha <diogo.f.cunha@gmail.com>
2017-03-29 19:19:41 +0100

    state added

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import _ from 'lodash'
  * @param update
  */
 function transverseFactory (update, step) {
-  return function transverse (dsl, obj, ctx) {
+  return function transverse (dsl, obj, state, ctx) {
     // If DSL node is function (an operator or other compiled DSL) then call it
     if (_.isFunction(dsl)) {
-      return dsl(obj, ctx)
+      return dsl(obj, state, ctx)
     }
     // Otherwise map the values of the DSL node by calling transverse on them and updating the context
-    return _.mapValues(dsl, (value, key) => transverse(dsl[key], obj[key], update(ctx, step({ key, dsl: dsl[key], obj: obj[key] }))))
+    return _.mapValues(dsl, (value, key) =>
+      transverse(dsl[key], obj[key], update(state, step({ key, dsl: dsl[key], obj: obj[key] })), ctx)
+    )
   }
 }
 
@@ -21,13 +23,13 @@ function transverseFactory (update, step) {
  * @param initCtx
  * @param update
  */
-export function compileFactory (operators, initCtx = {}, update = _.identity, step = _.noop) {
+export function compileFactory (operators, initState, update = _.identity, step = _.identity, initCtx = {}) {
   const transverse = transverseFactory(update, step)
   const boundOperators = _.mapValues(operators, operator => operator(update))
   return function compile (dslFn) {
     const dsl = _.isFunction(dslFn) ? dslFn(boundOperators) : dslFn
-    return function process (node, ctx = initCtx) {
-      return transverse(dsl, node, ctx)
+    return function process (node, state = initState, ctx = initCtx) {
+      return transverse(dsl, node, state, ctx)
     }
   }
 }
--- src/operators.js
+++ src/operators.js
@@ -1,4 +1,4 @@
 import _ from 'lodash'
 
-export const array = update => fn => (node, ctx) => _.map(node, (value, index) => fn(value, update(index, ctx)))
-export const object = update => fn => (node, ctx) => _.mapValues(node, (value, key) => fn(value, update(key, ctx)))
+export const array = update => fn => (node, state, ctx) => _.map(node, (value, index) => fn(value, update(index, state)))
+export const object = update => fn => (node, state, ctx) => _.mapValues(node, (value, key) => fn(value, update(key, state)))
